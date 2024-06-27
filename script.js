692263fe4540fc32018227ae536441c6ff50eb1d
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-06-27 13:22:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let score = 0
+
+console.log("score is " + score)
+// you can change the let value 
+let amount=1000
+console.log('your bill amount is Rs. '+ amount)
+const pi = 3.14 // you can't change the const value
+console.log(pi)
+var value = 123
+// var is also a variable but it is an outdated thing in js
